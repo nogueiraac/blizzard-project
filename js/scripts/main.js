1d5aa9c87193246ccfe5a29852afeb0682550a88
nogueiraac <240700ac@gmail.com>
2022-03-04 15:25:47 -0300

    Adding animations and styles in section games.

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -15,3 +15,23 @@ var slideHero = new Swiper(".main-slide", {
     disableOnInteraction: false,
   },
 });
+
+const allFilters = document.querySelectorAll(".js-nav-games li a");
+const tabPane = document.querySelectorAll(".tab-pane-games");
+
+allFilters.forEach((filter, index) => {
+  filter.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    allFilters.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    tabPane.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    tabPane[index].classList.add("active");
+    filter.classList.add("active");
+  });
+});
